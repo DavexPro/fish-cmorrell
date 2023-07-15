9eaae430670e258f1ecdc0fa48fbf07e28e806b5
Dave <dave.fang@outlook.com>
2023-07-15 11:22:55 +0800

    feat: update fish_prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -40,7 +40,7 @@ function show_user -d "Show user"
   if not contains $USER $default_user; or test -n "$SSH_CLIENT"
     set -l host (hostname -s)
     set -l who (whoami)
-    prompt_segment normal yellow " $who"
+    prompt_segment normal yellow "$who"
 
     # Skip @ bit if hostname == username
     if [ "$USER" != "$HOST" ]
@@ -71,7 +71,7 @@ end
 # Show prompt w/ privilege cue
 function show_prompt -d "Shows prompt with cue for current priv"
   set -l uid (id -u $USER)
-    if [ $uid -eq 0 ]
+  if [ $uid -eq 999 ]
     prompt_segment red white " ! "
     set_color normal
     echo -n -s " "
@@ -85,7 +85,7 @@ end
 ## SHOW PROMPT
 function fish_prompt
   set -g RETVAL $status
-  show_status
+  # show_status
   show_virtualenv
   show_user
   show_pwd
